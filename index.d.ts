7589fd60520fffad30067bf590f0a4ea651b72e3
AlexReloaded <alexhfokmh@gmail.com>
2017-03-09 10:43:45 +0800

    chore(update): from Phoenix framework

--- index.d.ts
+++ index.d.ts
@@ -3,21 +3,32 @@ export class Push {
 }
 
 export class Channel {
+  topic: string;
   state: string;
   joinPush: Push;
+  bindings: Array<Function>;
 
   constructor(topic: string, params: any, socket: Socket);
 
   join(): Push;
   leave(): Push;
+  rejoin(): void;
   on(eventName: string, callback: (payload: any) => void);
   off(eventName: string);
   onClose(callback: () => void);
   onError(callback: (reason: string) => void);
 }
 
+interface SocketOpts {
+  transport?: any;
+  timeout?: number;
+  logger?: (kind: string, msg: any, data: any) => void;
+  longpollerTimeout?: number;
+  params?: any;
+}
+
 export class Socket {
-  constructor(endPoint: string);
+  constructor(endPoint: string, opts?: SocketOpts);
 
   connect();
   channel(topic: string, params: any): Channel;
